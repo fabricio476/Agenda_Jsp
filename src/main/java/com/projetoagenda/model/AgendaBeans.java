90fa46aed4164c7995e3fe4081559f9b766bb515
Fabrício R. de França <fabriciobtcc@gmail.com>
2022-04-20 01:25:18 -0300

    classes e lib do driver para PERCISTENCIA DE DADOS

--- src/main/java/com/projetoagenda/model/AgendaBeans.java
+++ src/main/java/com/projetoagenda/model/AgendaBeans.java
@@ -1,5 +1,57 @@
 package com.projetoagenda.model;
 
 public class AgendaBeans {
+	
+	private int idcon;
+	private String nome;
+	private String fone;
+	private String email;
+	
+	public AgendaBeans() {
+		
+		
+	}
+	
+	
+	
+	
+	public AgendaBeans(int idcon, String nome, String fone, String email) {
+		super();
+		this.idcon = idcon;
+		this.nome = nome;
+		this.fone = fone;
+		this.email = email;
+	}
+
+
+
+
+	public int getIdcon() {
+		return idcon;
+	}
+	public void setIdcon(int idcon) {
+		this.idcon = idcon;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getFone() {
+		return fone;
+	}
+	public void setFone(String fone) {
+		this.fone = fone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
 
 }
